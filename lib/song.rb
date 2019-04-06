19cab4b497285b3c10bf87d45273a0f56b64ea45
Ryan Pethel <arpethel@gmail.com>
2019-04-06 18:38:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,7 @@ class Song
   end
 
   def self.find_by_name(name)
-    binding.pry
+    # binding.pry
     result = @@all.detect {|song| song.name = name}
     # @@all.each { |name| @song if name == @song }
     #   binding.pry
