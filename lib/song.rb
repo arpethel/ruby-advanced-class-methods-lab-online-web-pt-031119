2c29cd40e5244f54a2dd84f13e32a3a47337ba61
Ryan Pethel <arpethel@gmail.com>
2019-04-06 16:53:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,15 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+
+  # Use this constructor instead of #initialize. Constructors extend #initialize functionality
+  def self.create
+    @song = self.new
+    @song.save
+    @song
+  end
 
   def self.all
     @@all
@@ -9,5 +18,16 @@ class Song
   def save
     self.class.all << self
   end
+
+  def self.new_by_name(name)
+    @song.name
+    # binding.pry
+    # song = @song
+    # @name = name
+    # binding.pry
+    # song.name = name
+    # song.name
+  end
+
 
 end
