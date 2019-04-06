40b524a7ba31e0f3c79b22607274797330001237
Ryan Pethel <arpethel@gmail.com>
2019-04-06 17:08:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,8 +21,8 @@ class Song
 
   def self.new_by_name(name)
     # binding.pry
-    # @song = self.new
-    @song.name = name
+    @song = self.new(name)
+    # @song.name = name
   end
 
   def self.create_by_name
