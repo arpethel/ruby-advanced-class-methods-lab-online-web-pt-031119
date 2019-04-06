11f77c9552f2926468e3d7f0d032838c34024c4c
Ryan Pethel <arpethel@gmail.com>
2019-04-06 17:33:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,8 @@ class Song
     end
   end
 
-  def self.find_or_create_by_name
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
   end
 
   def self.alphabetical
